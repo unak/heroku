54e358d773395279addcada34dd35e743c430342
dickeyxxx <jeff@dickeyxxx.com>
2014-11-15 02:45:21 -0800

    small refactor

--- lib/heroku/jsplugin.rb
+++ lib/heroku/jsplugin.rb
@@ -2,8 +2,8 @@ class Heroku::JSPlugin
   include Heroku::Helpers
 
   def self.load!
-    bin = heroku_plugins_path
     return unless File.exists? bin
+    this = self
     plugins.each do |plugin|
       klass = Class.new do
         def initialize(args, opts)
@@ -13,7 +13,7 @@ class Heroku::JSPlugin
       end
       klass.send(:define_method, :run) do
         ENV['HEROKU_APP'] = @opts[:app]
-        exec bin, "#{plugin[:topic]}:#{plugin[:command]}"
+        exec this.bin, "#{plugin[:topic]}:#{plugin[:command]}"
       end
       Heroku::Command.register_namespace(name: plugin[:topic])
       Heroku::Command.register_command(
@@ -26,28 +26,28 @@ class Heroku::JSPlugin
   end
 
   def self.plugins
-    @plugins ||= `#{heroku_plugins_path} commands`.split.flat_map do |l|
+    @plugins ||= `#{bin} commands`.split.flat_map do |l|
       l.scan(/(\w+):(\w+)/).collect do |topic, command|
         {:topic => topic, :command => command }
       end
     end
   end
 
-  def self.heroku_plugins_path
+  def self.bin
     File.join(Heroku::Helpers.home_directory, ".heroku", "heroku-plugins")
   end
 
   def self.setup
-    return if File.exist? heroku_plugins_path
+    return if File.exist? bin
     $stderr.print 'setting up heroku-plugins...'
-    FileUtils.mkdir_p File.dirname(heroku_plugins_path)
+    FileUtils.mkdir_p File.dirname(bin)
     resp = Excon.get(url, middlewares: Excon.defaults[:middlewares] + [Excon::Middleware::Decompress])
-    open(heroku_plugins_path, "wb") do |file|
+    open(bin, "wb") do |file|
       file.write(resp.body)
     end
     File.chmod(0755, heroku_plugins_path)
-    if Digest::SHA1.file(heroku_plugins_path).hexdigest != manifest['builds'][os][arch]['sha1']
-      File.delete heroku_plugins_path
+    if Digest::SHA1.file(bin).hexdigest != manifest['builds'][os][arch]['sha1']
+      File.delete bin
       raise 'SHA mismatch for heroku-plugins'
     end
     $stderr.puts 'done'
